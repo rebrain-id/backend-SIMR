cba496afc04bba8fe63496a855b4dc2b3ae550de
Ardhi Harry <ardhiharry@gmail.com>
2024-08-24 22:48:27 +0700

    feat: Department | create featrue CRUD basic Department

--- src/modules/department/department.service.ts
+++ src/modules/department/department.service.ts
@@ -1,26 +1,115 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateDepartmentDto } from './dto/create-department.dto';
 import { UpdateDepartmentDto } from './dto/update-department.dto';
+import { PrismaService } from '../../prisma/prisma.service';
+import {
+  Department,
+  selectedFieldDepartment,
+} from './entities/department.entity';
 
 @Injectable()
 export class DepartmentService {
-  create(createDepartmentDto: CreateDepartmentDto) {
-    return 'This action adds a new department';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createDepartmentDto: CreateDepartmentDto): Promise<Department> {
+    const { name } = createDepartmentDto;
+
+    const exists = await this.prisma.department.findFirst({
+      where: { name },
+    });
+
+    if (exists) {
+      throw new HttpException('Department already exists', 400);
+    }
+
+    try {
+      const result = await this.prisma.department.create({
+        data: {
+          name,
+        },
+        select: selectedFieldDepartment(),
+      });
+
+      return result;
+    } catch (error) {
+      throw new HttpException('Failed create Department', 500);
+    }
   }
 
-  findAll() {
-    return `This action returns all department`;
+  async findAll(): Promise<Department[]> {
+    const result = await this.prisma.department.findMany({
+      select: selectedFieldDepartment(),
+    });
+
+    if (result.length === 0) {
+      throw new HttpException('Department not found', 404);
+    }
+
+    try {
+      return result;
+    } catch (error) {
+      throw new HttpException('Failed get all Department', 500);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} department`;
+  async findOne(uuid: string): Promise<Department> {
+    const result = await this.prisma.department.findUnique({
+      where: {
+        uuid,
+      },
+      select: selectedFieldDepartment(),
+    });
+
+    if (!result) {
+      throw new HttpException('Department not found', 404);
+    }
+
+    try {
+      return result;
+    } catch (error) {
+      throw new HttpException('Failed get Department', 500);
+    }
   }
 
-  update(id: number, updateDepartmentDto: UpdateDepartmentDto) {
-    return `This action updates a #${id} department`;
+  async update(
+    uuid: string,
+    updateDepartmentDto: UpdateDepartmentDto,
+  ): Promise<Department> {
+    const { name } = updateDepartmentDto;
+
+    const exists: Department = await this.prisma.department.findUnique({
+      where: { uuid },
+    });
+
+    if (!exists) throw new HttpException('Department not found', 404);
+
+    try {
+      const result: Department = await this.prisma.department.update({
+        where: { uuid },
+        data: {
+          name,
+        },
+        select: selectedFieldDepartment(),
+      });
+
+      return result;
+    } catch (error) {
+      throw new HttpException('Failed update Department', 500);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} department`;
+  async remove(uuid: string): Promise<string> {
+    const exists: Department = await this.prisma.department.findUnique({
+      where: { uuid },
+    });
+
+    if (!exists) throw new HttpException('Department not found', 404);
+
+    try {
+      await this.prisma.department.delete({ where: { uuid } });
+      return 'Success delete Department with uuid: ' + uuid;
+    } catch (error) {
+      throw new HttpException('Failed delete Department', 500);
+    }
   }
 }
--- src/modules/department/entities/department.entity.ts
+++ src/modules/department/entities/department.entity.ts
@@ -1,1 +1,16 @@
-export class Department {}
+export class Department {
+  id?: number;
+  uuid: string;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export function selectedFieldDepartment() {
+  return {
+    uuid: true,
+    name: true,
+    createdAt: true,
+    updatedAt: true,
+  };
+}
