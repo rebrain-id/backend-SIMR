1fe95e9083d1d566c99252d7851e4e23cc33465a
bayeng <88835193+bayeng@users.noreply.github.com>
2024-08-25 19:51:31 +0700

    feat: swager | add documentation for bayu domain

--- src/modules/detail-agenda/entities/detail-agendum.entity.ts
+++ src/modules/detail-agenda/entities/detail-agendum.entity.ts
@@ -1,13 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class DetailAgendum {
   id?: number;
+
+  @ApiProperty()
   uuid: string;
+
+  @ApiProperty()
   title: string;
+
+  @ApiProperty()
   typeAgendaId?: number;
+
+  @ApiProperty()
   typeAgenda?: any;
+
+  @ApiProperty()
   start: Date;
+
+  @ApiProperty()
   finish: Date;
+
+  @ApiProperty()
   description: string;
+
+  @ApiProperty()
   createdAt: Date;
+
+  @ApiProperty()
   updatedAt: Date;
 }
 
@@ -20,7 +40,20 @@ export function selectedFieldDetailAgenda() {
     finish: true,
     typeAgenda: {
       select: {
+        uuid: true,
         name: true,
+      },
+    },
+    agenda: {
+      select: {
+        lecturer: {
+          select: {
+            uuid: true,
+            name: true,
+            email: true,
+            phoneNumber: true,
+          },
+        },
       },
     },
     createdAt: true,
--- src/modules/type-agenda/dto/create-type-agendum.dto.ts
+++ src/modules/type-agenda/dto/create-type-agendum.dto.ts
@@ -1,6 +1,8 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTypeAgendumDto {
+  @ApiProperty({ example: 'rapat prodi' })
   @IsNotEmpty()
   @IsString()
   name: string;
