4439bcc61b7a542bb8cffe5a8fdc0f41b37c5638
bayeng <88835193+bayeng@users.noreply.github.com>
2024-08-25 19:51:01 +0700

    fix: agenda | fix bussines logic in create agenda and fix documentation swagger agenda

--- src/modules/agenda/dto/update-agenda.dto.ts
+++ src/modules/agenda/dto/update-agenda.dto.ts
@@ -1,4 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAgendaDto } from './create-agenda.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateAgendaDto extends PartialType(CreateAgendaDto) {}
+export class UpdateAgendaDto extends PartialType(CreateAgendaDto) {
+  @ApiProperty({ type: [String] })
+  @IsNotEmpty()
+  @IsString()
+  lecturerUuid: string[];
+
+  @ApiProperty({ type: [String] })
+  @IsNotEmpty()
+  @IsString()
+  detailAgendaUuid: string;
+}
