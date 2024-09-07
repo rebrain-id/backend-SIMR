4d6d2a79ec9cfe7911900860b060f598169c7627
bayeng <88835193+bayeng@users.noreply.github.com>
2024-09-07 12:46:17 +0700

    refactor: agenda | refactor docs and create new file for doc api agenda

--- src/modules/agenda/agenda.controller.ts
+++ src/modules/agenda/agenda.controller.ts
@@ -12,6 +12,7 @@ import { CreateAgendaDto } from './dto/create-agenda.dto';
 import { Response } from '../../helper/response';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CheckAgendaDto } from './dto/check-agenda.dto';
+import { checkExistAgenda } from './doc/agenda.doc';
 
 @ApiTags('Agenda')
 @Controller('agendas')
@@ -21,34 +22,7 @@ export class AgendaController {
   @HttpCode(HttpStatus.OK)
   @Version('1')
   @Post('check')
-  @ApiResponse({
-    status: HttpStatus.OK,
-    type: CheckAgendaDto,
-    example: {
-      statusCode: 200,
-      message: 'Success check exist agenda',
-      data: [
-        {
-          status: 'conflict schedule',
-          lecturerUuid: '318dafdf-fdca-4a76-8042-e781e02d9b3f',
-          name: 'Tiffany Zieme',
-          detailAgendaUuid: '09de4ce7-9acc-4d57-a86d-7a81deb98e40',
-          titleAgenda: 'Rapat Paripurna testing brp ini suksesss harusnya',
-          start: '2024-05-20T05:00:00.000Z',
-          finish: '2024-06-20T06:30:00.000Z',
-        },
-        {
-          status: 'conflict schedule',
-          lecturerUuid: '3a93ca1d-8ced-4096-b17e-297601490928',
-          name: 'Daniel Volkman',
-          detailAgendaUuid: '09de4ce7-9acc-4d57-a86d-7a81deb98e40',
-          titleAgenda: 'Rapat Paripurna testing brp ini suksesss harusnya',
-          start: '2024-05-20T05:00:00.000Z',
-          finish: '2024-06-20T06:30:00.000Z',
-        },
-      ],
-    },
-  })
+  @ApiResponse(checkExistAgenda())
   async checkExistAgenda(@Body() checkAgendaDto: CheckAgendaDto) {
     try {
       const result = await this.agendaService.checkExistAgenda(checkAgendaDto);
--- src/modules/detail-agenda/detail-agenda.controller.ts
+++ src/modules/detail-agenda/detail-agenda.controller.ts
@@ -5,7 +5,6 @@ import {
   Get,
   HttpStatus,
   Param,
-  ParseFilePipeBuilder,
   Patch,
   Post,
   Query,
@@ -24,6 +23,11 @@ import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
+import {
+  createDetailAgendaDoc,
+  findAllDetailAgendaDoc,
+  findOneDetailAgendaDoc,
+} from './doc/detail-agenda.doc';
 
 @ApiTags('Detail Agenda')
 @Controller('detail-agendas')
@@ -32,35 +36,7 @@ export class DetailAgendaController {
 
   @Version('1')
   @Post()
-  @ApiResponse({
-    status: 201,
-    description: 'Success create detail agenda',
-    example: {
-      statusCode: 201,
-      message: 'Success create detail agenda',
-      data: {
-        uuid: '73a3e3cc-ab51-41aa-b68a-a7a4045ce41e',
-        title: 'Rapat Anggaran Rebrain',
-        description:
-          'Rapat ini berisi tentang RebrainStudio yang akan menjadi vendor Unmuh Jember',
-        location: 'CC Lt 4.2',
-        start: '2024-05-20T05:00:00.000Z',
-        finish: '2024-06-20T06:30:00.000Z',
-        typeAgenda: {
-          uuid: '57de51a5-7dc5-4e0d-8242-3e00431097c0',
-          name: 'rapat Eksternal coy',
-        },
-        departments: [
-          {
-            uuid: 'ae4daf7c-39f9-4c7c-89d2-d6e2727bdbee',
-            name: 'S1 Informatika',
-          },
-        ],
-        createdAt: '2024-08-29T16:16:39.218Z',
-        updatedAt: '2024-08-29T16:16:39.218Z',
-      },
-    },
-  })
+  @ApiResponse(createDetailAgendaDoc())
   async create(@Body() createDetailAgendumDto: any): Promise<Response> {
     try {
       const result: DetailAgendums = await this.detailAgendaService.create(
@@ -81,41 +57,7 @@ export class DetailAgendaController {
 
   @Version('1')
   @Get()
-  @ApiResponse({
-    status: 200,
-    description: 'Success get all detail agenda',
-    type: DetailAgendums,
-    example: {
-      statusCode: 200,
-      message: 'Success get all detail agenda',
-      data: [
-        {
-          uuid: '09de4ce7-9acc-4d57-a86d-7a81deb98e40',
-          title: 'Rapat Paripurna testing brp ini suksesss harusnya',
-          start: '2024-05-20T05:00:00.000Z',
-          finish: '2024-06-20T06:30:00.000Z',
-          typeAgenda: {
-            uuid: '57de51a5-7dc5-4e0d-8242-3e00431097c0',
-            name: 'rapat Eksternal coy',
-          },
-          location: 'CC Lt 4.2',
-          author: 'informatika',
-        },
-        {
-          uuid: '45e7b993-e9dd-43eb-bf48-c3cdd4af2881',
-          title: 'Rapat Paripurna testing brp ini suksesss harusnya',
-          start: '2024-05-20T05:00:00.000Z',
-          finish: '2024-06-20T06:30:00.000Z',
-          typeAgenda: {
-            uuid: '57de51a5-7dc5-4e0d-8242-3e00431097c0',
-            name: 'rapat Eksternal coy',
-          },
-          location: 'CC Lt 4.2',
-          author: 'informatika',
-        },
-      ],
-    },
-  })
+  @ApiResponse(findAllDetailAgendaDoc())
   async findAll(@Query('username') username: string): Promise<Response> {
     try {
       const result: DetailAgendums[] =
@@ -135,35 +77,7 @@ export class DetailAgendaController {
 
   @Version('1')
   @Get(':uuid')
-  @ApiResponse({
-    status: 200,
-    description: 'Success get detail agenda',
-    type: DetailAgendum,
-    example: {
-      statusCode: 200,
-      message: 'Success get detail agenda',
-      data: {
-        uuid: '45e7b993-e9dd-43eb-bf48-c3cdd4af2881',
-        title: 'Rapat Paripurna testing brp ini suksesss harusnya',
-        description: 'coba 123',
-        start: '2024-05-20T05:00:00.000Z',
-        finish: '2024-06-20T06:30:00.000Z',
-        typeAgenda: {
-          uuid: '57de51a5-7dc5-4e0d-8242-3e00431097c0',
-          name: 'rapat Eksternal coy',
-        },
-        departments: [
-          {
-            uuid: 'ae4daf7c-39f9-4c7c-89d2-d6e2727bdbee',
-            name: 'S1 Informatika',
-          },
-        ],
-        location: 'CC Lt 4.2',
-        createdAt: '2024-08-29T07:26:20.063Z',
-        updatedAt: '2024-08-29T07:26:20.077Z',
-      },
-    },
-  })
+  @ApiResponse(findOneDetailAgendaDoc())
   async findOne(@Param('uuid') uuid: string): Promise<Response> {
     try {
       const result: DetailAgendum =
