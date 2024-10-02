4e1edfbf4b08ac37cde99fc5e286b08cfd874e56
Ardhi Harry <ardhiharry@gmail.com>
2024-10-02 16:20:17 +0700

    fix: detail-agenda | update response findOne detail-agenda

--- src/modules/detail-agenda/detail-agenda.service.ts
+++ src/modules/detail-agenda/detail-agenda.service.ts
@@ -526,6 +526,7 @@ export class DetailAgendaService {
       },
       include: {
         typeAgenda: true,
+        user: true,
       },
     });
 
@@ -559,6 +560,7 @@ export class DetailAgendaService {
         name: detailAgenda.typeAgenda.name,
       },
       departments: dataDeparments,
+      author: detailAgenda.user.username,
       isDone: detailAgenda.isDone,
       notulen: detailAgenda.notulen,
       absent: detailAgenda.absent,
